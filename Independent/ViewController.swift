49d3bbe6a5d15f438aab341b6028b5f3e4ffb7c4
Ygeer <ygeer1926@gmail.com>
2018-04-09 11:25:44 -0400

    Initial Commit

---
+++ Independent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Independent
+//
+//  Created by Yoni Geer on 4/9/18.
+//  Copyright © 2018 Yoni Geer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
